9e1b0afac5adcf3c365e93df82399be51d138e66
greta novak <tonikaposik@gmail.com>
2022-10-27 18:14:43 +0300

    name

---
+++ mya.cpp
@@ -0,0 +1,30 @@
+﻿#include <windows.h>
+#include <iostream>
+#pragma comment(lib, "winmm.lib")
+int main()
+{
+    using namespace std;
+    const int n = 5;
+    int a[n];
+    for (int i = 0; i < n; i++)
+           cin >> a[i];
+    int c = 0;
+    double s = 0;
+    bool f = true;
+    for (int i = 0; i <= n / 2; i++)
+       if (a[i] != a[n - i - 1])        {
+           f = false;
+            break;
+       }
+    if (f) {
+      
+        cout << "Yes";
+        PlaySound(TEXT("C:\\temp\\yes.wav"), NULL, SND_FILENAME);
+    }
+    else {
+       
+        cout << "No";
+        PlaySound(TEXT("C:\\temp\\no.wav"), NULL, SND_FILENAME);
+    }
+}
+
